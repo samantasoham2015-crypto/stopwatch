4eab82768755452b48ad4a86a29af08026c7a0dc
samantasoham2015-crypto <samanta.soham2015@gmail.com>
2026-09-04 11:40:26 -0400

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,11 @@
+"use strict";
+
+self.addEventListener("install", () => self.skipWaiting());
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => Promise.all(
+      cacheNames.map((cacheName) => caches.delete(cacheName))
+    )).then(() => self.registration.unregister())
+  );
+});

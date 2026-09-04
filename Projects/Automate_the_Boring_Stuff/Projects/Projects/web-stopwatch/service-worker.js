1960baf736015aaf9781ce266d017f9f9ce4622f
Soham <samanta.soham2015@gmail.com>
2026-09-04 11:20:18 -0400

    Show stopwatch hundredths of a second

--- Projects/Automate_the_Boring_Stuff/Projects/Projects/web-stopwatch/service-worker.js
+++ Projects/Automate_the_Boring_Stuff/Projects/Projects/web-stopwatch/service-worker.js
@@ -1,10 +1,20 @@
 "use strict";
 
-const CACHE_NAME = "tracktime-v2";
+const CACHE_NAME = "tracktime-v3";
 const APP_FILES = ["./", "index.html", "styles.css", "app.js?v=2", "manifest.webmanifest"];
 
 self.addEventListener("install", (event) => {
-  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_FILES)));
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_FILES)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => Promise.all(
+      cacheNames.filter((cacheName) => cacheName !== CACHE_NAME).map((cacheName) => caches.delete(cacheName))
+    )).then(() => self.clients.claim())
+  );
 });
 
 self.addEventListener("fetch", (event) => {
